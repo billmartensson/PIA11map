f5def03cb2082d252ddaf825eac2d9fecbf9c87f
Bill Mårtensson <bill@magictechnology.se>
2022-11-21 09:13:03 +0100

    Initial Commit

---
+++ PIA11map/PIA11mapApp.swift
@@ -0,0 +1,17 @@
+//
+//  PIA11mapApp.swift
+//  PIA11map
+//
+//  Created by Bill Martensson on 2022-11-21.
+//
+
+import SwiftUI
+
+@main
+struct PIA11mapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
